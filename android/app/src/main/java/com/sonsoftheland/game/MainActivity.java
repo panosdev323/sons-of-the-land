5c2ca6a8b7c0dcaa4df91da31bb08c307f024bef
panosdev323 <130555016+panosdev323@users.noreply.github.com>
2026-05-18 23:23:58 +0300

    fix

--- android/app/src/main/java/com/sonsoftheland/game/MainActivity.java
+++ android/app/src/main/java/com/sonsoftheland/game/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sonsoftheland.game;
 
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // ✅ initialize Google Mobile Ads SDK
+        MobileAds.initialize(this);
     }
 }
